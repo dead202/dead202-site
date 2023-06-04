e5bce2e02c8acba1ebcef5c4c2746de7ad191d57
dead202 <60527213+dead202@users.noreply.github.com>
2023-06-04 22:48:57 +0300

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,7 +3,7 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="https://dead202.netlify.app" target={"_blank"}>
+			<a href="https://dead202.netlify.app" target={"https://dead202.netlify.app"}>
 				<img
 					className={styles.alchemy_logo}
 					src="/alchemy_logo.svg"
