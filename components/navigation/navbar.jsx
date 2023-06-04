3b4384adcd7ca622d93a13d65eefc4eb890935aa
dead202 <60527213+dead202@users.noreply.github.com>
2023-06-04 23:04:53 +0300

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -1,5 +1,4 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
