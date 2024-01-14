7ec73a47bd5b787c65be41048ef1142b6670065b
dead202 <60527213+dead202@users.noreply.github.com>
2024-01-14 18:13:45 +0200

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -1,13 +1,12 @@
 // Navbar.jsx
 import React from 'react';
 import styles from './Navbar.module.css'; // Stil dosyasını import et
-import logo from './HD - Logo - PNG.png'; // Logo dosyasını import et
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a href="/">
-        <img className={styles.logo} src={logo} alt="Logo" />
+        <img className={styles.logo} src="https://r.resimlink.com/wB2JTSEf.png" alt="Logo" />
       </a>
       {/* Diğer Navbar öğeleri buraya eklenebilir */}
     </nav>
