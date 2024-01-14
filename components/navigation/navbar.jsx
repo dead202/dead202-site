7e5a31277247008be3c22933865c2c8c9dea910f
dead202 <60527213+dead202@users.noreply.github.com>
2024-01-14 17:58:46 +0200

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
 			<a href="https://dead202.netlify.app" target="_blank" rel="noopener noreferrer">
 				<img
 					src="/components/navigation/HD - Logo - PNG.png" // Logo dosyasının yolunu doğru şekilde ayarlayın
-					alt="Bu bir başlık" // Alt metni (alternate text) ekleyin
+					alt="" // Alt metni (alternate text) ekleyin
 					className="baslik-stili" // Başlık için doğru sınıf adını (class name) ekleyin
 				/>
 			</a>
