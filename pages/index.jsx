a11f5ad49a62f12508a9ecba1261f41b9a3d9667
dead202 <60527213+dead202@users.noreply.github.com>
2024-01-14 18:33:30 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,9 @@
 import styles from "../styles/Home.module.css";
-import NFTGallery from "../components/nftGallery";
 
 export default function Home() {
   return (
     <div>
       <main className={styles.main}>
-        <NFTGallery />
       </main>
     </div>
   );
